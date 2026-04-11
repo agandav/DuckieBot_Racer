aeec38719c49187ea90fa2a37e570d9b1ec7787d
Ruth Sugiarto <rsugiart@purdue.edu>
2026-04-11 19:43:19 +0000

    some fixes to tof

--- sensor_integration/ros-sensor-tof/packages/example_ros_tof/src/tof.py
+++ sensor_integration/ros-sensor-tof/packages/example_ros_tof/src/tof.py
@@ -7,12 +7,13 @@ from dt_robot_utils import get_robot_name
 
 SENSOR_NAME: str = "front_center"
 
-
+# This node listens to the ToF sensor topic and prints the distance to the console. if too far it will print "Too-far". This is useful for testing the ToF sensor and making sure it's working correctly. It can also be used as a starting point for more complex applications that use the ToF sensor data.
 def callback(msg: Range):
     distance: str = f"{msg.range:.3f}m" if msg.range < msg.max_range else "Too-far"
     print(f"Range: {distance}")
+    return distance
 
-
+#continuously runs the node to listen ot the tof sensor
 def listener():
     robot_name: str = get_robot_name()
     # initialize node
@@ -25,7 +26,6 @@ def listener():
     )
     # keep the node alive
     rospy.spin()
-
 
 if __name__ == '__main__':
     listener()
